2c7a5d8a688d3a21a64c9fc0647c27d08dce996c
ryan <ryandehallo229@gmail.com>
2021-04-14 04:17:58 -0300

    source/

---
+++ source/java_script.js
@@ -0,0 +1,6 @@
+function get_data() {
+    var email = document.getElementById("one")
+    var senha = document.getElementById("two")
+
+    window.alert(`User: ${email.value} \nPassword: ${senha.value}`)
+}
